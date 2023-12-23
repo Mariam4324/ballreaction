ee8eed2de807696648c8c28fbc6c39e19284376c
Mariam4324 <ashkmariam@gmail.com>
2023-12-23 22:38:37 +0300

    3 commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 const main = document.querySelector(".main");
+const ball = document.querySelector(".ball");
 
 main.addEventListener("click", (ev) => {
   ev.stopPropagation();
   if (ev.target.closest(".ball")) {
-    console.log("ball!");
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      const randomValue = Math.floor(Math.random() * (max - min + 1)) + min;
+      ball.style.top = `${randomValue}px`;
+    }
+
+    getRandomInt(1, 600);
   }
 });
