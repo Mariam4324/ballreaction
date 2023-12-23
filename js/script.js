85864152335a454ad10554c9b62dfc1072b3537b
Mariam4324 <ashkmariam@gmail.com>
2023-12-23 22:51:27 +0300

    4 commit

--- js/script.js
+++ js/script.js
@@ -7,10 +7,21 @@ main.addEventListener("click", (ev) => {
     function getRandomInt(min, max) {
       min = Math.ceil(min);
       max = Math.floor(max);
-      const randomValue = Math.floor(Math.random() * (max - min + 1)) + min;
-      ball.style.top = `${randomValue}px`;
+      const randomTopValue = Math.floor(Math.random() * (max - min + 1)) + min;
+
+      ball.style.top = `${randomTopValue}px`;
     }
 
     getRandomInt(1, 600);
+
+    function leftRandom(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+
+      const randomLeftValue = Math.floor(Math.random() * (max - min + 1)) + min;
+      ball.style.left = `${randomLeftValue}px`;
+    }
+
+    leftRandom(1, 1300);
   }
 });
