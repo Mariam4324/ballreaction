841ecfc77bdead96b54f4df4cbb46d60abec9d0b
Mariam4324 <ashkmariam@gmail.com>
2023-12-24 12:12:07 +0300

    7 commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const main = document.querySelector(".main");
 const ball = document.querySelector(".ball");
 
+let arrTimer = [];
+
 main.addEventListener("click", (ev) => {
   ev.stopPropagation();
   if (ev.target.closest(".ball")) {
@@ -22,37 +24,40 @@ main.addEventListener("click", (ev) => {
     }
     leftRandom(30, 1300);
 
-    // function timer() {
-    //   const start = new Date();
+    function timer() {
+      const start = new Date();
 
-    //   const a = start.getTime();
+      const a = start.getTime();
 
-    //   const end = new Date();
-    //   console.log(`Цикл занял: ${end - start} ms`);
-    // }
-    // timer();
-
-    let arrTimer = [];
+      const end = new Date();
+      console.log(`Цикл занял: ${end - start} ms`);
+    }
+    timer();
 
     function renderTime(milliseconds) {
-      const objTime = [
-        
-      ];
+      const objTime = {
+        id: arrTimer.length,
+        text: "passed:",
+      };
 
       const wrapper = document.querySelector(".time_wrapper");
       const item = document.createElement("div");
 
       item.innerHTML = ` 
-    <div class="time_item">
-      <span class="time_title">passed:</span>
+    <div id="${objTime.id}" class="time_item">
+      <span class="time_title">${objTime.text}</span>
       <span class="time">${milliseconds} ms</span>
     </div>`;
 
       wrapper.append(item);
+      arrTimer.push(objTime);
 
-      if (arrTimer.length > 5) {
+      if (arrTimer.length === 5) {
         arrTimer = [];
+        wrapper.innerHTML = "";
       }
+
+      console.log(arrTimer);
     }
     renderTime(1000);
   }
